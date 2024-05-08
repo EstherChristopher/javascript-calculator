d89b4a6fa46c517752dbcf98f6e871457c1232d9
Adamu Esther Christopher <estheradamuchris@gmail.com>
2024-05-08 01:58:28 +0100

    commit message

--- main.js
+++ main.js
@@ -1,57 +1,67 @@
-const display = document.getElementById('display');
+const display = document.getElementById("display");
 
-const numberButtons = document.querySelectorAll('.number');
+const numberButtons = document.querySelectorAll(".number");
 
-const operationsButtons = document.querySelectorAll('.operations');
+const operationsButtons = document.querySelectorAll(".operations");
 
-const signButtons = document.querySelectorAll('.sign');
+const signButtons = document.querySelectorAll(".sign");
 
-numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        console.log('Button clicked. Value:', button.textContent);
-        const number = button.textContent;
-        updateDisplay(number);
-    });
+// let basket = [];
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const number = button.textContent;
+    updateDisplay(number);
+    // localStorage.setItem('digit', number);
+  });
 });
 function updateDisplay(value) {
-    console.log('Display content:', display.textContent);
-    console.log('Value:', value);
-    if (display.textContent === '0') {
-        console.log('Condition met: Display content is 0');
-        console.log('Replacing 0 with', value);
-        display.textContent = value;
-    } 
-    else  {
-        console.log('Condition not met: Display content is not 0');
-        console.log('Appending', value);
-        display.textContent += value;
-    }
+  if (display.textContent === "0") {
+    display.textContent = value;
+    // const initialValue = display.textContent;
+    // localStorage.setItem('digit', initialValue)
+  } else {
+    display.textContent += value;
+  }
+  let storedValue = localStorage.getItem("digit") || "";
+  storedValue += value;
+
+  localStorage.setItem("digit", storedValue);
+  // localStorage.setItem('digit', value)
 }
 
-signButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const sign = button.dataset.sign;
-        modifyDisplay(sign);
-    });
+signButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const sign = button.dataset.sign;
+    localStorage.setItem("operator", sign);
+  });
 });
-function modifyDisplay (value) {
-    if (display.textContent === '0') {
-        display.textContent = value;
-    } else {
-        display.textContent += value;
-    }
+function modifyDisplay(value) {
+  if (display.textContent === "0") {
+    display.textContent = value;
+  } else {
+    display.textContent += value;
+  }
 }
 
-operationsButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const operations = button.dataset.sign || button.textContent;
-        changeDisplay(operations)
-    })
-})
-function changeDisplay (value) {
-    if(display.textContent == '0') {
-        display.textContent = value;
-    } else {
-        display.textContent += value;
-    }
+operationsButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const operations = button.dataset.sign || button.textContent;
+    changeDisplay(operations);
+  });
+});
+function changeDisplay(value) {
+  if (display.textContent == "0") {
+    display.textContent = value;
+  } else {
+    display.textContent += value;
+  }
 }
+
+clearAll = () => {
+  button.addEventListener("click", () => {});
+};
+
+clearLast = () => {
+  button.addEventListener("click", () => {});
+};
